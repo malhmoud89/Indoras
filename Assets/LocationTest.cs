8d0292865151d82143929ea871db8ba7e295c26e
Mo Alhmoud <malhmoud@live.com>
2018-09-06 20:54:22 +0300

    InitialCommit

---
+++ Assets/LocationTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocationTest : MonoBehaviour {
+
+    Text myText;
+	// Use this for initialization
+	void Start () {
+        myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myText.text = string.Concat("Screen Resolution:", Screen.width, ",", Screen.height,
+            "\n","Mouse Position:",Input.mousePosition.x,",",Input.mousePosition.y,
+            "\n");
+	}
+    
+}
---
+++ Assets/move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class move : MonoBehaviour {
+
+    // Use this for initialization
+    Animator anim;
+    NavMeshAgent navMesh;
+    float hitdist = 0.0f;
+    Plane playerPlane;
+    Ray ray;
+    public Vector3 targetPoint;
+    void Start () {
+        navMesh = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerPlane = new Plane(Vector3.up, transform.position);
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        anim.SetBool("isRunning", navMesh.hasPath);
+
+        if (Input.GetMouseButton(0))
+        {
+
+
+            playerPlane.Raycast(ray, out hitdist);
+            targetPoint = ray.GetPoint(hitdist);
+            navMesh.SetDestination(targetPoint);
+
+
+
+
+
+        }
+    }
+}
